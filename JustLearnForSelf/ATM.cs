801eac943c635439bbd820adc9adc1347cf76952
Mehrshad-w <microsoftmehr@gmail.com>
2022-06-05 09:08:55 +0430

    Add project files.

---
+++ JustLearnForSelf/ATM.cs
@@ -0,0 +1,13 @@
+﻿namespace JustLearnForSelf
+{
+    public class ATM
+    {
+        public bool Start()
+        {
+            Bank bank =  new Bank();
+            IO io = new IO();
+            bool Flag = io.GetCommand();
+            return Flag;
+        }
+    }
+}
